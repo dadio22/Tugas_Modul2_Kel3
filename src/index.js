243ab9c0d14a8401e3a95ae198808a3b615074ba
dadio22 <49015286+dadio22@users.noreply.github.com>
2021-09-06 16:09:29 +0700

    Tugas Modul 2
    1 Component 3 Props (Dadio)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Produk from "./components/produk";
+
+const data = [
+  {
+    name: "Black Tee",
+    price: "$20",
+    color: "Black",
+    images:
+      "https://shop.lostfrequencies.com/wp-content/uploads/2020/03/Lost-on-the-Beach-Black-tee-back.jpg",
+  },
+  {
+    name: "White Tee",
+    price: "$20",
+    color: "White",
+    images:
+      "https://shop.lostfrequencies.com/wp-content/uploads/2020/03/Lost-Frequencies-white-tee-the-beach-back.jpg",
+  },
+  {
+    name: "Red Tee",
+    price: "$20",
+    color: "Red",
+    images:
+      "https://www.wrangler-ap.com/storage/app/uploads/public/5d2/408/686/5d2408686ebbb789429690.jpg",
+  },
+  {
+    name: "Green Tee",
+    price: "$20",
+    color: "Green",
+    images:
+      "https://ae01.alicdn.com/kf/Hf76f21200a284183b984d9ecd813c9563/Summer-Green-Vintage-T-Shirt-Boys-Dark-Green-Tee-Shirt-Cotton-Short-Sleeve-Quality-Blank-Tshirt.jpg",
+  },
+];
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Produk title="TOKO KELOMPOK 3" data={data} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
