ff34d76cac6e2667ca473f6037b1f562b6ab5bba
yisrel278 <yisrel278@gmail.com>
2021-09-07 19:46:07 +0700

    produk.js dipindah

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Produk from "./components/produk";
+import Produk from "./components/Produk/produk";
 
 const data = [
   {
