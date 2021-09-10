baf54e9e7f1ae153081b374ebc5dbf4f1fdc332c
dadio22 <49015286+dadio22@users.noreply.github.com>
2021-09-10 19:03:27 +0700

    logo

--- src/components/Produk/produk.js
+++ src/components/Produk/produk.js
@@ -4,13 +4,13 @@ export default function Produk(props) {
   return (
     <div className="bg-white">
       <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          <img
-            src={logo}
-            alt={logo}
-            className="w-full h-full object-center object-cover lg:w-16 lg:h-16"
-          />
-          <h2 className="text-4xl font-extrabold tracking-tight text-gray-900 flex justify-center">
+        <img
+          src={logo}
+          alt={logo}
+          className="w-full h-full object-center object-cover lg:w-16 lg:h-16"
+        />
+        <div className="mt-6 grid grid-rows-1">
+          <h2 className="text-center text-4xl font-extrabold tracking-tight text-gray-900 flex justify-items-center">
             {title}
           </h2>
         </div>
